5160ce0068066d46c1b7a1ef40551a3383ce1e96
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-03-11 00:49:41 +0800

    摸索的重构, 尽量减少一些副作用;

--- app/src/main/java/cn/misection/miscourse/activity/ExercisesDetailActivity.java
+++ app/src/main/java/cn/misection/miscourse/activity/ExercisesDetailActivity.java
@@ -22,18 +22,20 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExercisesDetailActivity extends AppCompatActivity {
-    private TextView tv_main_title, tv_back;
-    private RelativeLayout rl_title_bar;
-    private ListView lv_list;
+public class ExercisesDetailActivity extends AppCompatActivity
+{
+    private TextView MainTitleTextView, backTextView;
+    private RelativeLayout titleBarRelaLayout;
+    private ListView listView;
     private String title;
     private int id;
-    private List<ExercisesBean> eb1;
+    private List<ExercisesBean> beanList;
     private ExercisesDetailAdapter adapter;
 
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises_detail);
         // 设置此界面为竖屏
@@ -42,40 +44,49 @@ public class ExercisesDetailActivity extends AppCompatActivity {
         id = getIntent().getIntExtra("id", 0);
         // 获取从习题界面传递过来的章节标题
         title = getIntent().getStringExtra("title");
-        eb1 = new ArrayList<>();
+        beanList = new ArrayList<>();
         initData();
         init();
     }
 
-    private void init() {
-        tv_main_title = findViewById(R.id.tv_main_title);
-        tv_back = findViewById(R.id.text_view_back);
-        rl_title_bar = findViewById(R.id.title_bar);
-        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
-        lv_list = findViewById(R.id.lv_list);
+    private void init()
+    {
+        MainTitleTextView = findViewById(R.id.tv_main_title);
+        backTextView = findViewById(R.id.text_view_back);
+        titleBarRelaLayout = findViewById(R.id.title_bar);
+        titleBarRelaLayout.setBackgroundColor(Color.parseColor("#30B4FF"));
+        listView = findViewById(R.id.lv_list);
         TextView tv = new TextView(this);
         tv.setTextColor(Color.parseColor("#000000"));
         tv.setTextSize(16.0f);
         tv.setText("一、选择题");
         tv.setPadding(10, 15, 0, 0);
-        lv_list.addHeaderView(tv);
-        tv_main_title.setText(title);
-        tv_back.setOnClickListener(new View.OnClickListener() {
+        listView.addHeaderView(tv);
+        MainTitleTextView.setText(title);
+        backTextView.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 ExercisesDetailActivity.this.finish();
             }
         });
-        adapter = new ExercisesDetailAdapter(ExercisesDetailActivity.this, new ExercisesDetailAdapter.OnSelectListener() {
+        adapter = new ExercisesDetailAdapter(ExercisesDetailActivity.this, new ExercisesDetailAdapter.OnSelectListener()
+        {
             @Override
-            public void onSelectA(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+            public void onSelectA(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d)
+            {
                 // 判断如果答案不是 1 即 A 选项
-                if (eb1.get(position).getAnswer() != 1) {
-                    eb1.get(position).setSelect(1);
-                } else {
-                    eb1.get(position).setSelect(0);
+                if (beanList.get(position).getAnswer() != 1)
+                {
+                    beanList.get(position).setSelect(1);
                 }
-                switch (eb1.get(position).getAnswer()) {
+                else
+                {
+                    beanList.get(position).setSelect(0);
+                }
+                switch (beanList.get(position).getAnswer())
+                {
                     case 1:
                         iv_a.setImageResource(R.drawable.exercises_right_icon);
                         break;
@@ -96,14 +107,19 @@ public class ExercisesDetailActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onSelectB(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+            public void onSelectB(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d)
+            {
                 // 判断如果答案不是 2 即 B 选项
-                if (eb1.get(position).getAnswer() != 2) {
-                    eb1.get(position).setSelect(2);
-                } else {
-                    eb1.get(position).setSelect(0);
+                if (beanList.get(position).getAnswer() != 2)
+                {
+                    beanList.get(position).setSelect(2);
                 }
-                switch (eb1.get(position).getAnswer()) {
+                else
+                {
+                    beanList.get(position).setSelect(0);
+                }
+                switch (beanList.get(position).getAnswer())
+                {
                     case 1:
                         iv_a.setImageResource(R.drawable.exercises_right_icon);
                         iv_b.setImageResource(R.drawable.exercises_error_icon);
@@ -124,14 +140,19 @@ public class ExercisesDetailActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onSelectC(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+            public void onSelectC(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d)
+            {
                 // 判断如果答案不是 3 即 C 选项
-                if (eb1.get(position).getAnswer() != 3) {
-                    eb1.get(position).setSelect(3);
-                } else {
-                    eb1.get(position).setSelect(0);
+                if (beanList.get(position).getAnswer() != 3)
+                {
+                    beanList.get(position).setSelect(3);
                 }
-                switch (eb1.get(position).getAnswer()) {
+                else
+                {
+                    beanList.get(position).setSelect(0);
+                }
+                switch (beanList.get(position).getAnswer())
+                {
                     case 1:
                         iv_a.setImageResource(R.drawable.exercises_right_icon);
                         iv_c.setImageResource(R.drawable.exercises_error_icon);
@@ -152,14 +173,19 @@ public class ExercisesDetailActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onSelectD(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+            public void onSelectD(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d)
+            {
                 // 判断如果答案不是 4 即 D 选项
-                if (eb1.get(position).getAnswer() != 4) {
-                    eb1.get(position).setSelect(4);
-                } else {
-                    eb1.get(position).setSelect(0);
+                if (beanList.get(position).getAnswer() != 4)
+                {
+                    beanList.get(position).setSelect(4);
                 }
-                switch (eb1.get(position).getAnswer()) {
+                else
+                {
+                    beanList.get(position).setSelect(0);
+                }
+                switch (beanList.get(position).getAnswer())
+                {
                     case 1:
                         iv_a.setImageResource(R.drawable.exercises_right_icon);
                         iv_d.setImageResource(R.drawable.exercises_error_icon);
@@ -179,17 +205,23 @@ public class ExercisesDetailActivity extends AppCompatActivity {
                 AnalysisUtil.setABCDEnable(false, iv_a, iv_b, iv_c, iv_d);
             }
         });
-        adapter.setData(eb1);
-        lv_list.setAdapter(adapter);
+        adapter.setData(beanList);
+        listView.setAdapter(adapter);
     }
 
-    private void initData() {
-        try {
+    private void initData()
+    {
+        try
+        {
             InputStream is = getResources().getAssets().open("chapter" + id + ".xml");
-            eb1 = AnalysisUtil.getExercisesInfos(is);
-        } catch (IOException e) {
+            beanList = AnalysisUtil.getExercisesInfos(is);
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     }
---
+++ app/src/main/java/cn/misection/miscourse/model/MineModel.java
@@ -0,0 +1,19 @@
+package cn.misection.miscourse.model;
+
+import java.util.List;
+
+/**
+ * @author Military Intelligence 6 root
+ * @version 1.0.0
+ * @ClassName MineModel
+ * @Description TODO
+ * @CreateTime 2021年03月11日 00:45:00
+ */
+public class MineModel implements IModel
+{
+    @Override
+    public List requireData()
+    {
+        return null;
+    }
+}
