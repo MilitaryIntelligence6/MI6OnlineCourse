74517c6d3f3724ab904119a93dcef7c67ec1693c
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-03-10 22:17:47 +0800

    摸索的重构;

--- app/src/main/java/cn/misection/miscourse/adapter/VideoListAdapter.java
+++ app/src/main/java/cn/misection/miscourse/adapter/VideoListAdapter.java
@@ -14,87 +14,139 @@ import cn.misection.miscourse.bean.VideoBean;
 
 import java.util.List;
 
-public class VideoListAdapter extends BaseAdapter {
+public class VideoListAdapter extends BaseAdapter
+{
     private Context context;
-    private List<VideoBean> vb1;  // 视频列表数据
-    private int selectedPosition = -1;  // 点击时选中的位置
+
+    /**
+     * 视频列表数据;
+     */
+    private List<VideoBean> videoBeanList;
+
+    /**
+     * 点击时选中的位置;
+     */
+    private int selectedPosition = -1;
+
     private OnSelectListener onSelectListener;
 
-    public VideoListAdapter(Context context, OnSelectListener onSelectListener) {
+    public VideoListAdapter(Context context, OnSelectListener onSelectListener)
+    {
         this.context = context;
         this.onSelectListener = onSelectListener;
     }
 
-    public void setSelectedPosition(int position) {
+    public void setSelectedPosition(int position)
+    {
         this.selectedPosition = position;
     }
 
-    // 设置数据更新界面
-    public void setData(List<VideoBean> vb1) {
-        this.vb1 = vb1;
+    /**
+     * 设置数据更新界面;
+     * @param videoBeanList list;
+     */
+    public void setData(List<VideoBean> videoBeanList)
+    {
+        this.videoBeanList = videoBeanList;
         notifyDataSetChanged();
     }
 
     @Override
-    public int getCount() {
-        return vb1 == null ? 0 : vb1.size();
+    public int getCount()
+    {
+        return videoBeanList == null ? 0 : videoBeanList.size();
     }
 
     @Override
-    public VideoBean getItem(int position) {
-        return vb1 == null ? null : vb1.get(position);
+    public VideoBean getItem(int position)
+    {
+        return videoBeanList == null ? null : videoBeanList.get(position);
     }
 
     @Override
-    public long getItemId(int position) {
+    public long getItemId(int position)
+    {
         return position;
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        final ViewHolder vh;
-        if (convertView == null) {
-            vh = new ViewHolder();
+    public View getView(final int position, View convertView, ViewGroup parent)
+    {
+        final ViewHolder viewHolder;
+        if (convertView == null)
+        {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(context).inflate(R.layout.video_list_item, null);
-            vh.tv_title = convertView.findViewById(R.id.tv_video_title);
-            vh.iv_icon = convertView.findViewById(R.id.iv_left_icon);
-            convertView.setTag(vh);
-        } else {
-            vh = ((ViewHolder) convertView.getTag());
+            viewHolder.setTitleTextView((TextView) convertView.findViewById(R.id.tv_video_title));
+            viewHolder.setIconImageView((ImageView) convertView.findViewById(R.id.iv_left_icon));
+            convertView.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder = ((ViewHolder) convertView.getTag());
         }
         final VideoBean bean = getItem(position);
-        vh.iv_icon.setImageResource(R.drawable.course_bar_icon);
-        vh.tv_title.setTextColor(Color.parseColor("#333333"));
-        if (bean != null) {
-            vh.tv_title.setText(bean.getSecondTitle());
+        viewHolder.getIconImageView().setImageResource(R.drawable.course_bar_icon);
+        viewHolder.getTitleTextView().setTextColor(Color.parseColor("#333333"));
+        if (bean != null)
+        {
+            viewHolder.getTitleTextView().setText(bean.getSecondTitle());
             // 设置选中效果
-            if (selectedPosition == position) {
-                vh.iv_icon.setImageResource(R.drawable.course_intro_icon);
-                vh.tv_title.setTextColor(Color.parseColor("#009958"));
-            } else {
-                vh.iv_icon.setImageResource(R.drawable.course_bar_icon);
-                vh.tv_title.setTextColor(Color.parseColor("#333333"));
+            if (selectedPosition == position)
+            {
+                viewHolder.getIconImageView().setImageResource(R.drawable.course_intro_icon);
+                viewHolder.getTitleTextView().setTextColor(Color.parseColor("#009958"));
+            }
+            else
+            {
+                viewHolder.getIconImageView().setImageResource(R.drawable.course_bar_icon);
+                viewHolder.getTitleTextView().setTextColor(Color.parseColor("#333333"));
             }
         }
-        convertView.setOnClickListener(new View.OnClickListener() {
+        convertView.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                if (bean == null) {
+            public void onClick(View v)
+            {
+                if (bean == null)
+                {
                     return;
                 }
                 // 播放视频
-                onSelectListener.onSelect(position, vh.iv_icon);
+                onSelectListener.onSelect(position, viewHolder.getIconImageView());
             }
         });
         return convertView;
     }
 
-    class ViewHolder {
-        public TextView tv_title;
-        public ImageView iv_icon;
+    class ViewHolder
+    {
+        private TextView titleTextView;
+        private ImageView iconImageView;
+
+        public TextView getTitleTextView()
+        {
+            return titleTextView;
+        }
+
+        public void setTitleTextView(TextView titleTextView)
+        {
+            this.titleTextView = titleTextView;
+        }
+
+        public ImageView getIconImageView()
+        {
+            return iconImageView;
+        }
+
+        public void setIconImageView(ImageView iconImageView)
+        {
+            this.iconImageView = iconImageView;
+        }
     }
 
-    public interface OnSelectListener {
+    public interface OnSelectListener
+    {
         void onSelect(int position, ImageView iv);
     }
 }
