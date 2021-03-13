434989818cc5886f20bb127c4b4c4b381cdad90f
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-03-13 15:34:23 +0800

    重构, 原来的代码宛如shit;

--- app/src/main/java/cn/misection/miscourse/ui/adapter/CourseAdapter.java
+++ app/src/main/java/cn/misection/miscourse/ui/adapter/CourseAdapter.java
@@ -13,6 +13,7 @@ import cn.misection.miscourse.R;
 
 import cn.misection.miscourse.ui.activity.VideoListActivity;
 import cn.misection.miscourse.entity.CourseBean;
+import cn.misection.miscourse.ui.adapter.adapterconst.EnumImageResMapper;
 import cn.misection.miscourse.ui.adapter.holder.CourseViewHolder;
 
 import java.util.List;
@@ -94,7 +95,7 @@ public class CourseAdapter extends BaseAdapter
                     {
                         viewHolder.getLeftImgTitleTextView().setText(bean.getImgTitle());
                         viewHolder.getLeftTitleTextView().setText(bean.getTitle());
-                        setLeftImg(bean.getId(), viewHolder.getLeftImageView());
+                        putLeftImg(bean.getId(), viewHolder.getLeftImageView());
                         viewHolder.getLeftImageView().setOnClickListener(new View.OnClickListener()
                         {
                             @Override
@@ -114,7 +115,7 @@ public class CourseAdapter extends BaseAdapter
                     {
                         viewHolder.getRightImgTitleTextView().setText(bean.getImgTitle());
                         viewHolder.getRightTitleTextView().setText(bean.getTitle());
-                        setRightImg(bean.getId(), viewHolder.getRightImageView());
+                        putRightImg(bean.getId(), viewHolder.getRightImageView());
                         viewHolder.getRightImageView().setOnClickListener(new View.OnClickListener()
                         {
                             @Override
@@ -144,56 +145,20 @@ public class CourseAdapter extends BaseAdapter
      * @param id
      * @param rightImageView
      */
-    private void setRightImg(int id, ImageView rightImageView)
+    private void putRightImg(int id, ImageView rightImageView)
     {
-        switch (id)
-        {
-            case 2:
-                rightImageView.setImageResource(R.drawable.chapter_2_icon);
-                break;
-            case 4:
-                rightImageView.setImageResource(R.drawable.chapter_4_icon);
-                break;
-            case 6:
-                rightImageView.setImageResource(R.drawable.chapter_6_icon);
-                break;
-            case 8:
-                rightImageView.setImageResource(R.drawable.chapter_8_icon);
-                break;
-            case 10:
-                rightImageView.setImageResource(R.drawable.chapter_10_icon);
-                break;
-            default:
-            {
-                break;
-            }
-        }
+        rightImageView.setImageResource(
+                EnumImageResMapper.valueOf(id).getImgRes());
     }
 
-    // 设置左边图片
-    private void setLeftImg(int id, ImageView leftImageView)
+    /**
+     * 设置左边图片;
+     * @param id
+     * @param leftImageView
+     */
+    private void putLeftImg(int id, ImageView leftImageView)
     {
-        switch (id)
-        {
-            case 1:
-                leftImageView.setImageResource(R.drawable.chapter_1_icon);
-                break;
-            case 3:
-                leftImageView.setImageResource(R.drawable.chapter_3_icon);
-                break;
-            case 5:
-                leftImageView.setImageResource(R.drawable.chapter_5_icon);
-                break;
-            case 7:
-                leftImageView.setImageResource(R.drawable.chapter_7_icon);
-                break;
-            case 9:
-                leftImageView.setImageResource(R.drawable.chapter_9_icon);
-                break;
-            default:
-            {
-                break;
-            }
-        }
+        leftImageView.setImageResource(
+                EnumImageResMapper.valueOf(id).getImgRes());
     }
 }
---
+++ app/src/main/java/cn/misection/miscourse/ui/adapter/adapterconst/EnumImageResMapper.java
@@ -0,0 +1,57 @@
+package cn.misection.miscourse.ui.adapter.adapterconst;
+
+import cn.misection.miscourse.R;
+
+/**
+ * @author Military Intelligence 6 root
+ * @version 1.0.0
+ * @ClassName ImageResourceMapper
+ * @Description TODO
+ * @CreateTime 2021年03月13日 15:11:00
+ */
+public enum EnumImageResMapper
+{
+    /**
+     * 图片资源映射;
+     * 0 暂时占位;
+     */
+    IMAGE_0(R.drawable.chapter_1_icon),
+
+    IMAGE_1(R.drawable.chapter_1_icon),
+
+    IMAGE_2(R.drawable.chapter_2_icon),
+
+    IMAGE_3(R.drawable.chapter_3_icon),
+
+    IMAGE_4(R.drawable.chapter_4_icon),
+
+    IMAGE_5(R.drawable.chapter_5_icon),
+
+    IMAGE_6(R.drawable.chapter_6_icon),
+
+    IMAGE_7(R.drawable.chapter_7_icon),
+
+    IMAGE_8(R.drawable.chapter_8_icon),
+
+    IMAGE_9(R.drawable.chapter_9_icon),
+
+    IMAGE_10(R.drawable.chapter_10_icon),
+    ;
+
+    private final int imgRes;
+
+    EnumImageResMapper(int imgRes)
+    {
+        this.imgRes = imgRes;
+    }
+
+    public int getImgRes()
+    {
+        return imgRes;
+    }
+
+    public static EnumImageResMapper valueOf(int ordinal)
+    {
+        return values()[ordinal];
+    }
+}
